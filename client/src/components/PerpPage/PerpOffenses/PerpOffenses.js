840ab1c13ebba75b74077f056cca121efd2a2344
Paul Aglipay <PAglipay@ad.it.ucla.edu>
2022-03-09 17:43:44 -0800

    Adjusted layout to PerpPage lg=7/4 Col

--- client/src/components/PerpPage/PerpOffenses/PerpOffenses.js
+++ client/src/components/PerpPage/PerpOffenses/PerpOffenses.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import { Table, Button } from 'react-bootstrap';
+import { Table, Button, ButtonGroup } from 'react-bootstrap';
 
 function PerpOffenses({ hProps, data }) {
 
     async function deleteOffense(id) {
         const response = await fetch(`http://localhost:8083/p2/offense/delete/id?id=${id}`, {
-          method: "DELETE",
+            method: "DELETE",
         });
         console.log(`Deleting ${id}`);
         //window.location.reload(false);
@@ -25,10 +25,10 @@ function PerpOffenses({ hProps, data }) {
             <Table striped hover variant="dark" size="sm">
                 <thead>
                     <tr>
-                        {/* <th>
-                            controls
-                        </th> */}
                         {hProps.map((c, j) => (<th key={j}>{c}</th>))}
+                        <th>
+                            controls
+                        </th>
                     </tr>
                 </thead>
                 <tbody>
@@ -37,8 +37,13 @@ function PerpOffenses({ hProps, data }) {
                         return (<tr key={i}>
                             {/* <td><Button varient="danger" size="sm">Del</Button></td> */}
                             {hProps.map((c, j) => (<td key={`${c}-${j}`}>{e[c]}</td>))}
-                            <td><Button className={"btn-primary"} variant="flat">Edit</Button></td>
-                            <td><Button className={"btn-primary"} variant="flat" onClick={() => deleteOffense(e.id)}>Delete</Button></td></tr>)
+                            <td>
+                                <ButtonGroup>
+                                    <Button className={"btn-primary"} variant="flat">Edit</Button>
+                                    <Button className={"btn-primary"} variant="danger" onClick={() => deleteOffense(e.id)}>{JSON.stringify(e)}Delete</Button>
+                                </ButtonGroup>
+                            </td>
+                        </tr>)
                     })}
                 </tbody>
             </Table>
